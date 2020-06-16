bac15886bf9c7154f80da1772dfadfb67ea0e01e
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2020-06-16 17:51:58 +0700

    add scroll and fix menu, change features section

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,3 +4,32 @@ $('#nav-btn').on('click', function(){
     $("#nav").toggleClass('nav-active');
 });
 
+
+//smooth scroll
+$('[data-scroll]').on('click', function(event){
+    event.preventDefault();
+
+    var elementOffset;
+    var $this = $(this); 
+    var elementId = $this.data("scroll");
+    console.log(elementId);
+    if(elementId === "#intro-content"){
+        elementOffset = 0;
+    }else{
+        elementOffset = $(elementId).offset().top - $('#header').innerHeight();
+    }
+    
+    $('#nav div').removeClass('nav-item-active');
+    $this.addClass('nav-item-active');
+    
+    $('html, body').animate({
+        scrollTop: elementOffset
+    }, 500);
+
+    //$('.logo').removeClass('nav-active');
+    
+    // if($("#nav-toggle").hasClass('toggle-active')){
+    //     $('#nav-toggle').toggleClass('toggle-active');
+    //     $("#nav").toggleClass('nav-a-active');
+    // }
+});
